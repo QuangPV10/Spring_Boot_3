1b246ff35ac0dbe120d40d0be90d674455951d2a
Phạm Văn Quang <90913961+QuangPV10@users.noreply.github.com>
2024-11-15 22:40:10 +0700

    jwt part 1

--- src/main/java/com/example/demo/exception/ErrorCode.java
+++ src/main/java/com/example/demo/exception/ErrorCode.java
@@ -6,7 +6,9 @@ public enum ErrorCode {
     USER_NOT_FOUND(1002, "User not found"),
     USERNAME_INVALID(1003, "UserName must be at least 3 character"),
     PASSWORD_INVALID(1004, "Password must be at least 8 characters"),
-    USER_NOT_EXISTED(1005,"User not existed");
+    USER_NOT_EXISTED(1005,"User not existed"),
+    UNAUTHENTICATED(1006,"Unauthenticated")
+    ;
 
     private int code;
     private String message;
