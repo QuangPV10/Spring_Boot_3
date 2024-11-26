c8fb56a390b57ba47a6818c80ab74f73a897b1c7
Phạm Văn Quang <90913961+QuangPV10@users.noreply.github.com>
2024-11-26 22:04:41 +0700

    custom a annotation

--- src/main/java/com/example/demo/enums/ErrorCode.java
+++ src/main/java/com/example/demo/enums/ErrorCode.java
@@ -13,11 +13,12 @@ public enum ErrorCode {
     INVALID_KEY(1000, "Invalid message key", HttpStatus.INTERNAL_SERVER_ERROR),
     USER_EXISTED(1001,"User existed", HttpStatus.BAD_REQUEST),
     USER_NOT_FOUND(1002, "User not found", HttpStatus.BAD_REQUEST),
-    USERNAME_INVALID(1003, "UserName must be at least 3 character", HttpStatus.BAD_REQUEST),
-    PASSWORD_INVALID(1004, "Password must be at least 8 characters", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1003, "UserName must be at least {min} character", HttpStatus.BAD_REQUEST),
+    PASSWORD_INVALID(1004, "Password must be at least {min} characters", HttpStatus.BAD_REQUEST),
     USER_NOT_EXISTED(1005,"User not existed", HttpStatus.NOT_FOUND),
     UNAUTHENTICATED(1006,"Unauthenticated", HttpStatus.UNAUTHORIZED),
-    UNAUTHORIZED(1007,"You do not have permission", HttpStatus.FORBIDDEN)
+    UNAUTHORIZED(1007,"You do not have permission", HttpStatus.FORBIDDEN),
+    INVALID_DOB(1008,"Your age must be at least {min}", HttpStatus.BAD_REQUEST),
     ;
 
     int code;
--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -2,15 +2,21 @@ package com.example.demo.exception;
 
 import com.example.demo.dto.request.ApiResponse;
 import com.example.demo.enums.ErrorCode;
+import jakarta.validation.ConstraintViolation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
 import java.util.Objects;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+
+    private static final String MIN_ATTRIBUTE = "min";
 
     @ExceptionHandler(value = RuntimeException.class)
     ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException runtimeException) {
@@ -24,8 +30,13 @@ public class GlobalExceptionHandler {
     ResponseEntity<ApiResponse> handlingValidation(MethodArgumentNotValidException e) {
         String enumKey = Objects.requireNonNull(e.getFieldError()).getDefaultMessage();
         ErrorCode errorCode = ErrorCode.INVALID_KEY;
+        Map<String, Object> attributes = null;
         try {
             errorCode = ErrorCode.valueOf(enumKey);
+            ConstraintViolation constrainViolation = e.getBindingResult().getAllErrors().getFirst().unwrap(ConstraintViolation.class);
+            attributes = constrainViolation.getConstraintDescriptor().getAttributes();
+
+            log.info(attributes.toString());
         } catch (IllegalArgumentException ignored) {
 
         }
@@ -33,7 +44,7 @@ public class GlobalExceptionHandler {
         ApiResponse<?> apiResponse = new ApiResponse<>();
 
         apiResponse.setCode(errorCode.getCode());
-        apiResponse.setMessage(errorCode.getMessage());
+        apiResponse.setMessage(Objects.nonNull(attributes) ? mapAttribute(errorCode.getMessage(),attributes) : errorCode.getMessage());
         return ResponseEntity.badRequest().body(apiResponse);
     }
 
@@ -45,6 +56,11 @@ public class GlobalExceptionHandler {
         apiResponse.setCode(errorCode.getCode());
         apiResponse.setMessage(errorCode.getMessage());
         return ResponseEntity.status(errorCode.getHttpStatusCode()).body(apiResponse);
+    }
+
+    private String mapAttribute(String message, Map<String, Object> attributes) {
+        String minValue = attributes.get(MIN_ATTRIBUTE).toString();
+        return message.replace("{" + MIN_ATTRIBUTE + "}", minValue);
     }
 
 }
