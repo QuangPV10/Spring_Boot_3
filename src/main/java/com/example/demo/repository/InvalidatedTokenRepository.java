067ce322a787ea9ff49951927730579e6c06d49a
Phạm Văn Quang <90913961+QuangPV10@users.noreply.github.com>
2024-12-01 20:50:19 +0700

    write cron job to delete jwt expired

--- src/main/java/com/example/demo/repository/InvalidatedTokenRepository.java
+++ src/main/java/com/example/demo/repository/InvalidatedTokenRepository.java
@@ -2,8 +2,14 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.InvalidatedToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface InvalidatedTokenRepository extends JpaRepository<InvalidatedToken, String> {
+
+    @Query(value = "SELECT t FROM InvalidatedToken t WHERE t.expiryTime = (SELECT MIN(t2.expiryTime) FROM InvalidatedToken t2)")
+    List<InvalidatedToken> findAllByMinExpiryTime();
 }
